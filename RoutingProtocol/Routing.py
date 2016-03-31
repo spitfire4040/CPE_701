a4803dd874f7983e51f4ed93d5f97ca9dc6f9d32
Sifat Ibne Mushfique <sifatoooo@gmail.com>
2016-03-30 17:53:47 -0700

    Add a function to return the next hop.

--- RoutingProtocol/Routing.py
+++ RoutingProtocol/Routing.py
@@ -10,11 +10,11 @@ import Link
 
 # function: route table
 
-	"""
-	This command prints the local routing table at the node. It must show the
-	corresponding next-hop and the cost of the route in terms of number of hops.
-	"""
-	
+"""
+This command prints the local routing table at the node. It must show the
+corresponding next-hop and the cost of the route in terms of number of hops.
+"""
+
 def route_table(node):
 	links = node.GetLinks()
 	tabl = node.linkTable
@@ -81,3 +81,59 @@ def route_table(node):
 
 	#pass
 
+def next_hop(node):
+	links = node.GetLinks()
+	tabl = node.linkTable
+	for k in tabl.keys():
+		if (k!=int(node.nid)):
+			metrix1 = 1
+			tn1 = int(node.nid)
+			tn2 = links[0][0]
+			while tn2!=k:				
+				for key, value in tabl.items():
+					if key==tn2:
+						if len(filter(bool, value))==1:
+							metrix1 = 100
+							break
+						else:
+							if value[0]==tn1:
+								tn1 = tn2
+								tn2 = value[1]
+								metrix1 += 1
+							else:
+								tn1 = tn2
+								tn2 = value[0]
+								metrix1 += 1
+							#print tn2
+						break
+				if metrix1>10:
+					break
+			metrix2 = 1
+			tn1 = int(node.nid)
+			tn2 = links[1][0]
+			while tn2!=k:				
+				for key, value in tabl.items():
+					if key==tn2:
+						if len(filter(bool, value))==1:
+							metrix2 = 100
+							break
+						else:
+							if value[0]==tn1:
+								tn1 = tn2
+								tn2 = value[1]
+								metrix2 += 1
+							else:
+								tn1 = tn2
+								tn2 = value[0]
+								metrix2 += 1
+							#print tn2
+						break
+				if metrix2>10:
+					break
+			if metrix2>metrix1: 
+				metrix = metrix1
+				nh = links[0][0]
+			else: 
+				metrix = metrix2
+				nh = links[1][0]
+	return nh
